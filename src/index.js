ec3aec36ff821dceee536d0c135a13e6447acf8f
Jonas Wanner <jonas@upscore.ch>
2020-06-06 19:12:52 +0200

    add support for position relative containers and adding new examples and updating readme file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as helper from './helper'
+import * as debug from './debug'
 
 window.follow = {
     debug: {
@@ -9,6 +9,7 @@ window.follow = {
     attribute: 'data-follow',
     defaultFactor: 10,
     init: undefined,
+    destroy: undefined,
     animate: undefined
 }
 
@@ -16,8 +17,8 @@ follow.init = () => {
     // get all targets with attribute
     let targets = document.querySelectorAll(`[${follow.attribute}]`)
 
-    // debug
-    helper.log('targets', targets)
+    // log
+    debug.log('targets', targets)
 
     for (let target of targets) {
         // set the specified or the default factor
@@ -39,23 +40,80 @@ follow.init = () => {
             },
         }
 
+        // define absolute dimensions of the element
+        let bodyRectangular = document.body.getBoundingClientRect()
+        let elemRect = element.target.getBoundingClientRect()
+        let offsetLeft   = elemRect.left - bodyRectangular.left
+        let offsetTop   = elemRect.top - bodyRectangular.top
+
         // define dimensions of the element
         element.dimensions.height = element.target.offsetHeight
         element.dimensions.width = element.target.offsetWidth
 
         // define the center of the element as initial position
-        element.initialPosition.x = element.target.offsetLeft + (element.dimensions.width / 2)
-        element.initialPosition.y = element.target.offsetTop + (element.dimensions.height / 2)
+        element.initialPosition.x = offsetLeft + (element.dimensions.width / 2)
+        element.initialPosition.y = offsetTop + (element.dimensions.height / 2)
 
         // if center helper is wanted
-        helper.dot(element.initialPosition.x, element.initialPosition.y, 'red', 10000)
+        debug.dot(element.initialPosition.x, element.initialPosition.y, 'red', 10000)
+
+        // if container has position relative, remove element and add back on top of all
+        let parent = element.target.parentElement
+
+        while (true) {
+            if (parent.tagName === 'BODY') {
+                break
+            }
+            if (getComputedStyle(parent).position === 'relative') {
+                // clone element
+                let absoluteElement = element.target.cloneNode(true)
+
+                // give to the new element position absolute
+                absoluteElement.style.position = 'absolute'
+
+                // position element to exact spot
+                absoluteElement.style.left = element.initialPosition.x - (element.dimensions.width / 2) + 'px'
+                absoluteElement.style.top = element.initialPosition.y - (element.dimensions.height / 2) + 'px'
+
+                // append absolute element to the body
+                document.body.append(absoluteElement)
+
+                // remove old element
+                element.target.remove()
+
+                // add new element as new target
+                element.target = absoluteElement
+                break
+            }
+            parent = parent.parentElement
+        }
 
         // push element to array
         follow.elements.push(element)
     }
 
-    // debug
-    helper.log('elements', follow.elements)
+    // activate event listener
+    document.addEventListener('mousemove', follow.animate)
+
+    // log
+    debug.log('elements', follow.elements)
+}
+
+follow.destroy = () => {
+    // log
+    debug.log('follow.destroy called')
+
+    // remove the event listener
+    document.removeEventListener('mousemove', follow.animate)
+
+    // set the initial position for all elements
+    for (const element of follow.elements) {
+        element.target.style.left = element.initialPosition.x - (element.dimensions.width / 2) + 'px'
+        element.target.style.top = element.initialPosition.y - (element.dimensions.height / 2) + 'px'
+    }
+
+    // log
+    debug.log('follow.destroy() finished')
 }
 
 follow.animate = (event) => {
@@ -70,36 +128,35 @@ follow.animate = (event) => {
     }
 
     // add dot for mouse
-    helper.dot && helper.dot(mouseX, mouseY, 'blue')
+    debug.dot(mouseX, mouseY, 'blue')
 
-    // debug
-    helper.log('mouseX', mouseX)
-    helper.log('mouseY', mouseY)
+    // log
+    debug.log('mouseX', mouseX)
+    debug.log('mouseY', mouseY)
 
     for (let element of follow.elements) {
-        // debug
-        helper.log('element', element)
+        // log
+        debug.log('element', element)
 
         // calculate additional pixels
         let additionalX = (mouseX - element.initialPosition.x) / element.factor
         let additionalY = (mouseY - element.initialPosition.y) / element.factor
 
         // calculate future position
-        let futureX = (element.initialPosition.x + additionalX) - (element.dimensions.width / 2)
-        let futureY = (element.initialPosition.y + additionalY) - (element.dimensions.height / 2)
-
-        // set future position
-        element.target.style.left = futureX + 'px'
-        element.target.style.top = futureY + 'px'
+        let futureX = (element.initialPosition.x + additionalX)
+        let futureY = (element.initialPosition.y + additionalY)
 
         // add helper dot if wanted
-        helper.dot && helper.dot(futureX, futureY, 'green')
+        debug.dot(futureX, futureY, 'green')
 
-        // debug
-        helper.log('future position x', futureX)
-        helper.log('future position y', futureY)
+        // set future position
+        element.target.style.left = futureX - (element.dimensions.width / 2) + 'px'
+        element.target.style.top = futureY - (element.dimensions.height / 2) + 'px'
+
+        // log
+        debug.log('future position x', futureX)
+        debug.log('future position y', futureY)
     }
 }
 
 follow.init()
-document.addEventListener('mousemove', follow.animate)
