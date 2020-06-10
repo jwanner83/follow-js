99ffbf8c87c641182e13d26f16d3ea4eaae066ba
jwa <jonas.wanner@clavisit.com>
2020-06-10 14:27:01 +0200

    first working prototype of new calculation

--- src/helper.js
+++ src/helper.js
@@ -1,27 +1,19 @@
-export function clone (element) {
-    // clone element
-    let absolute = element.target.cloneNode(true)
+export function getPosition(target, center = true) {
+    // define absolute location of element
+    let bodyRectangular = document.body.getBoundingClientRect()
+    let elemRect = target.getBoundingClientRect()
+    let x = elemRect.left - bodyRectangular.left
+    let y = elemRect.top - bodyRectangular.top
 
-    // give to the new element position absolute
-    absolute.style.position = 'absolute'
+    // if position should be center of element get center
+    if (center) {
+        // define dimensions of the element
+        let height = target.offsetHeight
+        let width = target.offsetWidth
 
-    // add the correct dimensions to the element
-    absolute.style.height = element.dimensions.height + 'px'
-    absolute.style.width = element.dimensions.width + 'px'
+        x += (width / 2)
+        y += (height / 2)
+    }
 
-    // position element to exact spot
-    absolute.style.left = element.initialPosition.x - (element.dimensions.width / 2) + 'px'
-    absolute.style.top = element.initialPosition.y - (element.dimensions.height / 2) + 'px'
-
-    // append absolute element to the body
-    document.body.append(absolute)
-
-    // hide old element but not remove it to keep the space
-    element.target.style.opacity = '0'
-
-    // save element as before to add it back
-    element.before = element.target
-
-    // add new element as new target
-    element.target = absolute
+    return {x: x, y: y}
 }
