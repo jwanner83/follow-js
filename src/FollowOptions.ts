f0e26f4dbe1e461e3834a2aa6419b167c8c8ea7d
jwa <jonas.wanner@clavisit.com>
2021-03-18 21:02:54 +0100

    adding types to options and add factor for mobile

--- src/FollowOptions.ts
+++ src/FollowOptions.ts
@@ -7,29 +7,36 @@ export default class FollowOptions {
    * The factor how much the element moves with your cursor
    * @type {number}
    */
-  public factor = 10
+  public factor: number = 10
 
   /**
    * The attribute for the elements you want to follow
    * @type {string}
    */
-  public attribute = 'data-follow'
+  public attribute: string = 'data-follow'
 
   /**
    * If the object should automatically initiate the script on initialization of the class
    * @type {boolean}
    */
-  public initiate = true
+  public initiate: boolean = true
 
   /**
    * If the elements should follow on mobile with the gyro sensor
    * @type {boolean}
    */
-  public mobile = true
+  public mobile: boolean = true
+
+  /**
+   * The factor how fast the mobile fake cursor should move.
+   * Higher equals more movement for all elements
+   * @type {number}
+   */
+  public mobileFactor: number = 20
 
   /**
    * If debug mode is activated with log messages and visual helpers
    * @type {boolean}
    */
-  public debug = false
+  public debug: boolean = false
 }
