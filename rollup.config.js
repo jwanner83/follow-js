c8692f9b27bccf9013387d6080dc74d11d60bc1d
jwa <jonas.wanner@clavisit.com>
2021-03-30 08:51:41 +0200

    adding terser to rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/Follow.ts',
   plugins: [
-    typescript()
+    typescript(),
+    terser()
   ],
   output: [
     {
